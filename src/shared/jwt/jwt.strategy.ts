ef17cac5a2050ee5de49f7bcf933ed03dd741b78
yebin <yebin5127@gmail.com>
2023-07-04 16:23:46 +0900

    Create: jwt strategy 추가

---
+++ src/shared/jwt/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Repository } from "typeorm";
+import { User } from "src/entities/user.entity";
+import { UnAuthorizedError } from "../exception";
+import { Payload } from "./jwt.payload";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_KEY,
+            ignoreExpiration: false
+        });
+    }
+
+    async validate(payload: Payload) {
+        const user = await this.userRepository.findOne({
+            where: { user_id: payload.userId }
+        });
+
+        if(user) {
+            return user;
+        } else {
+            throw new UnAuthorizedError;
+        }
+    }
+}
