77e7bfd28307a4ef80360f379ac12ef9914807d1
yebin <yebin5127@gmail.com>
2023-07-04 16:20:52 +0900

    Create: exception 추가

---
+++ src/shared/exception.ts
@@ -0,0 +1,53 @@
+export class HttpError extends Error {
+    public statusCode: number;
+  
+    public message: string;
+  
+    constructor(statusCode: number, message: string) {
+      super(message);
+      this.statusCode = statusCode;
+      this.message = message;
+    }
+  }
+  
+  export class UnAuthorizedError extends HttpError {
+    constructor() {
+      super(401, 'Unauthorized');
+    }
+  }
+  
+  export class ConflictError extends HttpError {
+    constructor(message: string) {
+      super(409, message);
+    }
+  }
+  
+  export class ExpiredTokenError extends HttpError {
+    constructor() {
+      super(401, 'Expired Token');
+    }
+  }
+  
+  export class ForbiddenError extends HttpError {
+    constructor() {
+      super(403, 'Forbidden Request');
+    }
+  }
+  
+  export class NotFoundError extends HttpError {
+    constructor() {
+      super(404, `Not Found`);
+    }
+  }
+  
+  export class BadRequestError extends HttpError {
+    constructor(message: string) {
+      super(400, message);
+    }
+  }
+  
+  export class InternalServerError extends HttpError {
+    constructor() {
+      super(500, 'Interal Server Error');
+    }
+  }  
