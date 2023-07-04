b0cb45770544c31b779ff89e728d380c141a9c41
yebin <yebin5127@gmail.com>
2023-07-04 17:05:21 +0900

    Create: taxi entity 설정

---
+++ src/entities/taxi.entity.ts
@@ -0,0 +1,16 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Way } from "./way.entityt";
+import { User } from "./user.entity";
+
+@Entity('taxi')
+export class Taxi {
+    @PrimaryColumn()
+    taxi_id: number;
+
+    @OneToMany(() => Way, (way) => way.user)
+    way: Way[];
+
+    @OneToOne(() => User, (user) => user.taxi, { nullable: false })
+    @JoinColumn({ name: 'taxi_id' })
+    user: User;
+}
