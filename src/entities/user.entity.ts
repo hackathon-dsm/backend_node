26cd7113955db1c15df95a342e88bcda334708aa
yebin <yebin5127@gmail.com>
2023-07-04 17:05:17 +0900

    Create: user entity 설정

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Way } from "./way.entityt";
+import { Taxi } from "./taxi.entity";
+import { Visitor } from "./visitor.entity";
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn()
+    user_id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at: Date;
+
+    @OneToMany(() => Way, (way) => way.user)
+    way: Way[];
+
+    @OneToOne(() =>  Taxi, (taxi) => taxi.user)
+    taxi: Taxi[];
+
+    @OneToOne(() =>  Visitor, (visitor) => visitor.user)
+    visitor: Visitor[];
+}
