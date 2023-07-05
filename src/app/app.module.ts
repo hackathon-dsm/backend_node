6cf44ff9284a20389744f7e76622cdc9de982f41
yebin <yebin5127@gmail.com>
2023-07-05 16:56:48 +0900

    Update: taxi 모듈 불러오기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'src/config/ormconfig';
 import { CallModule } from 'src/call/call.module';
 import { UserModule } from 'src/user/user.module';
+import { TaxiModule } from 'src/taxi/taxi.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
     AuthModule,
     CallModule,
-    UserModule
+    UserModule,
+    TaxiModule
   ],
   controllers: [AppController],
   providers: [AppService],
