7c657f31df7ef91e8188d865ee1f3300dadc54c2
yebin <yebin5127@gmail.com>
2023-07-05 13:25:58 +0900

    Update: 서버 로그 남기기

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module'; 
+import { AppModule } from './app/app.module';
+import morgan from 'morgan';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(morgan('dev'));
+  await app.listen(PORT);
+  console.log(PORT + "번 포트에서 대기 중");
 }
+
 bootstrap();
